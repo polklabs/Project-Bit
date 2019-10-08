820257ef2fd1cf618caefd95c7bc907af1413fdc
Andrew Polk <polkandrew01@gmail.com>
2019-10-08 12:49:10 -0700

    Implement 4 bit adder in code

--- Assets/Scripts/IntegratedCircuits/PQ130.cs
+++ Assets/Scripts/IntegratedCircuits/PQ130.cs
@@ -1,6 +1,8 @@
 using System.Runtime.Serialization;
 
-
+/**
+ * 4 Bit Full Adder
+ */
 namespace IntegratedCircuits
 {
     public class PQ130G : IntegratedCircuit
@@ -11,44 +13,48 @@ namespace IntegratedCircuits
             IcType = ICType.ic4;
             ModelName = "IC16";
 
-            PinModes[1] = PinMode.Ouput;
-            PinModes[2] = PinMode.Ouput;
-            PinModes[4] = PinMode.Ouput;
-            PinModes[5] = PinMode.Ouput;
-            PinModes[6] = PinMode.Ouput;
-            PinModes[10] = PinMode.Ouput;
-            PinModes[11] = PinMode.Ouput;
-            PinModes[13] = PinMode.Ouput;
-            PinModes[14] = PinMode.Ouput;
+            PinModes[1]     = PinMode.Ouput;
+            PinModes[2]     = PinMode.Ouput;
+            PinModes[4]     = PinMode.Ouput;
+            PinModes[5]     = PinMode.Ouput;
+            PinModes[6]     = PinMode.Ouput;
+            PinModes[10]    = PinMode.Ouput;
+            PinModes[11]    = PinMode.Ouput;
+            PinModes[13]    = PinMode.Ouput;
+            PinModes[14]    = PinMode.Ouput;
         }
 
         protected override void InternalUpdate()
         {
+            BitArray a = new BitArray(new bool[4] 
+            { 
+              PinState[4]   == State.HIGH ? true : false,
+              PinState[2]   == State.HIGH ? true : false,
+              PinState[13]  == State.HIGH ? true : false,
+              PinState[11]  == State.HIGH ? true : false
+            });
             BitArray b = new BitArray(new bool[4] 
             { 
-              
+              PinState[5]   == State.HIGH ? true : false,
+              PinState[1]   == State.HIGH ? true : false,
+              PinState[14]  == State.HIGH ? true : false,
+              PinState[10]  == State.HIGH ? true : false
             });
             int[] ab = new int[2];
-            //Inputs
-            Chip.Input[0] = PinState[10] == State.HIGH ? true : false;
-            Chip.Input[1] = PinState[11] == State.HIGH ? true : false;
-
-            Chip.Input[2] = PinState[14] == State.HIGH ? true : false;
-            Chip.Input[3] = PinState[13] == State.HIGH ? true : false;
-
-            Chip.Input[4] = PinState[1] == State.HIGH ? true : false;
-            Chip.Input[5] = PinState[2] == State.HIGH ? true : false;
-
-            Chip.Input[6] = PinState[5] == State.HIGH ? true : false;
-            Chip.Input[7] = PinState[4] == State.HIGH ? true : false;
-            Chip.Input[8] = PinState[6] == State.HIGH ? true : false;
-
+         
+            a.copyTo(ab, 0);
+            b.copyTo(ab, 1);
+         
+            int output = ab[0] + ab[1] + (PinState[6] == State.HIGH ? 1 : 0);
+         
+            BitArray bitOut = new BitArray(new byte[] { output });
+         
             //Outputs
-            PinState[8] = Chip.Output[0] ? State.HIGH : State.LOW;
-            PinState[9] = Chip.Output[1] ? State.HIGH : State.LOW;
-            PinState[12] = Chip.Output[2] ? State.HIGH : State.LOW;
-            PinState[0] = Chip.Output[3] ? State.HIGH : State.LOW;
-            PinState[3] = Chip.Output[4] ? State.HIGH : State.LOW;
+            PinState[8]     = bitOut[4] ? State.HIGH : State.LOW; //C4
+            PinState[3]     = bitOut[0] ? State.HIGH : State.LOW; //S1
+            PinState[0]     = bitOut[1] ? State.HIGH : State.LOW; //S2
+            PinState[12]    = bitOut[2] ? State.HIGH : State.LOW; //S3
+            PinState[9]     = bitOut[3] ? State.HIGH : State.LOW; //S4
         }
 
     }
