3cff4170c702d80a0706acdfde40a6853c704e78
Andrew Polk <polkandrew01@gmail.com>
2019-10-08 12:34:15 -0700

    Create PQ130.cs

---
+++ Assets/Scripts/IntegratedCircuits/PQ130.cs
@@ -0,0 +1,55 @@
+using System.Runtime.Serialization;
+
+
+namespace IntegratedCircuits
+{
+    public class PQ130G : IntegratedCircuit
+    {
+
+        public PQ130G() : base(16)
+        {
+            IcType = ICType.ic4;
+            ModelName = "IC16";
+
+            PinModes[1] = PinMode.Ouput;
+            PinModes[2] = PinMode.Ouput;
+            PinModes[4] = PinMode.Ouput;
+            PinModes[5] = PinMode.Ouput;
+            PinModes[6] = PinMode.Ouput;
+            PinModes[10] = PinMode.Ouput;
+            PinModes[11] = PinMode.Ouput;
+            PinModes[13] = PinMode.Ouput;
+            PinModes[14] = PinMode.Ouput;
+        }
+
+        protected override void InternalUpdate()
+        {
+            BitArray b = new BitArray(new bool[4] 
+            { 
+              
+            });
+            int[] ab = new int[2];
+            //Inputs
+            Chip.Input[0] = PinState[10] == State.HIGH ? true : false;
+            Chip.Input[1] = PinState[11] == State.HIGH ? true : false;
+
+            Chip.Input[2] = PinState[14] == State.HIGH ? true : false;
+            Chip.Input[3] = PinState[13] == State.HIGH ? true : false;
+
+            Chip.Input[4] = PinState[1] == State.HIGH ? true : false;
+            Chip.Input[5] = PinState[2] == State.HIGH ? true : false;
+
+            Chip.Input[6] = PinState[5] == State.HIGH ? true : false;
+            Chip.Input[7] = PinState[4] == State.HIGH ? true : false;
+            Chip.Input[8] = PinState[6] == State.HIGH ? true : false;
+
+            //Outputs
+            PinState[8] = Chip.Output[0] ? State.HIGH : State.LOW;
+            PinState[9] = Chip.Output[1] ? State.HIGH : State.LOW;
+            PinState[12] = Chip.Output[2] ? State.HIGH : State.LOW;
+            PinState[0] = Chip.Output[3] ? State.HIGH : State.LOW;
+            PinState[3] = Chip.Output[4] ? State.HIGH : State.LOW;
+        }
+
+    }
+}
